dcbd19a75d3763364a33ef4f669f8ccd79073597
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-02-19 11:16:48 +0100

    Bug fixer sur les versions

---
+++ backend/apps/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework.serializers import ModelSerializer
+
+from .models import Category, Notes, User
+
+class CategorySerializer(ModelSerializer):
+    
+    class Meta:
+        model = Category
+        fields = "__all__"
+
+
+class NotesSerializer(ModelSerializer):
+    
+    class Meta:
+        model = Notes
+        fields = "__all__"
+
+class UserSerializer(ModelSerializer):
+    
+    class Meta:
+        model = User
+        fields = [""]
+
+
