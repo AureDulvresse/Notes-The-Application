08521102b5386f0ea03cfe3c7688b9b64dd89827
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-02-20 14:32:49 +0100

    Update Dashboard.jsx

--- frontend/src/components/dashboard/Dashboard.jsx
+++ frontend/src/components/dashboard/Dashboard.jsx
@@ -1,4 +1,5 @@
 import StatBox from "./StatBox";
+import {Line} from "react-chartjs-2"
 
 const Dashboard = () => {
   return (
@@ -6,6 +7,7 @@ const Dashboard = () => {
       <div className="flex flex-col md:grid md:grid-cols-2 gap-3">
         <div className="shadow-sm px-4 flex-wrap py-2 bg-slate-200 dark:bg-slate-500 rounded-md h-[200px] md:h-full">
           <h4 className="text-slate-400 dark:text-slate-700">Titre</h4>
+
         </div>
         <div className="flex flex-col md:grid md:grid-cols-1 gap-3">
           <div className="flex flex-col md:grid md:grid-cols-2 gap-3">
